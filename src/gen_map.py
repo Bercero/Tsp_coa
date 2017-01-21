7da0858ace20923574b911b926dee1924faa2500
Andres Lopez Soleto <gordilocuente@gmail.com>
2017-01-21 19:18:31 +0100

    commit de seguridad antes de cambiar unas cuantas cosas

--- src/gen_map.py
+++ src/gen_map.py
@@ -3,71 +3,81 @@ from random import random
 from math import hypot
 from pickle import dump
 from os import walk
+
+
 class Mapa:
-    def __init__(self,nc):
-        self.escala=1000 #ancho y largo del mapa en kilometros
-        self.agujeros=False #TODO
-        self.simetrico=True #TODO
-                
-        #lista de nc ciudades con cordenadas aleatorias
-        self.cords=[{'x':None,'y':None} for i in range(nc)]
+    def __init__(self, nc):
+        self.escala = 1000  # ancho y largo del mapa en kilometros
+        self.agujeros = False  # TODO
+        self.simetrico = True  # TODO
+
+        # lista de nc ciudades con cordenadas aleatorias
+        self.cords = [{'x': None, 'y': None} for i in range(nc)]
         for i in range(nc):
-            self.cords[i]['x']=int(random()*self.escala)
-            self.cords[i]['y']=int(random()*self.escala)
-            
-        #calculo de la matriz de distacias
-        #TODO solo el tipo simple
-        self.dist=[[0]*nc for i in range(nc)]
+            self.cords[i]['x'] = int(random() * self.escala)
+            self.cords[i]['y'] = int(random() * self.escala)
+
+        # calculo de la matriz de distacias
+        # TODO solo el tipo simple
+        self.m_dist = [[0] * nc for i in range(nc)]
         for i in range(nc):
-            for j in range(i+1,nc):
-                x=self.cords[i]['x']-self.cords[j]['x']
-                y=self.cords[i]['y']-self.cords[j]['y']
-                self.dist[i][j]=hypot(x,y)
-                self.dist[j][i]=self.dist[i][j]     
-        #almacenamiento del objeto en un archivo para su uso posterior
+            for j in range(i + 1, nc):
+                x = self.cords[i]['x'] - self.cords[j]['x']
+                y = self.cords[i]['y'] - self.cords[j]['y']
+                self.m_dist[i][j] = hypot(x, y)
+                self.m_dist[j][i] = self.m_dist[i][j]
+        # almacenamiento del objeto en un archivo para su uso posterior
         self.guardar()
+
     def guardar(self):
-        #TODO pasar variable de entorno para encontrar la carpeta mapas
-        #TODO guardar como mapas/n_ciudades/simetrico_agujeros_1.mp
-        prefix='mapas/'
-        fichero=prefix+'mapa.mp'
+        # TODO pasar variable de entorno para encontrar la carpeta mapas
+        # TODO guardar como mapas/n_ciudades/simetrico_agujeros_1.mp
+        prefix = 'mapas/'
+        fichero = prefix + 'mapa.mp'
 
         ficheros = []
         for (path, dirs, files) in walk(prefix):
-             ficheros.extend(files)
-             break
+            ficheros.extend(files)
+            break
         print(ficheros)
-        with open(fichero,'wb') as f:
-             dump(self,f)
-    #TODO cambiar nombre por __print__ o algo asi
+        with open(fichero, 'wb') as f:
+            dump(self, f)
+
+    def get_distancia(self, a, b):
+        return self.m_dist[a][b]
+    # TODO cambiar nombre por __print__ o algo asi
     def pib(self):
         for i in self.cords:
             print(i)
-        #for i in self.dist:            print(i)     
-    
+        for i in self.m_dist:
+            print(i)
+
     def get_num_ciudades(self):
         return len(self.cords)
+
+
 if __name__ == '__main__':
     from sys import argv
-#try:
-    #el primer argumento es el numero de ciudades
-    nc=int(argv[1])
-    m=Mapa(nc)
+
+    # try:
+    # el primer argumento es el numero de ciudades
+    nc = int(argv[1])
+    m = Mapa(nc)
     m.pib()
-    
-#except:
-    #TODO mejorar esta linea y devolver error
+
+    # except:
+    # TODO mejorar esta linea y devolver error
     print('''USO:
-    '''+argv[0]+''' num_ciudades agujeros simetrico
-    ejemplo '''+argv[0]+''' 5 1 1
+    ''' + argv[0] + ''' num_ciudades agujeros simetrico
+    ejemplo ''' + argv[0] + ''' 5 1 1
         crea un mapa de 5 ciudades no todas ellas directamente conectadas y con caminos simetricos''')
-    
-#lee parametros
+
+# TODO hacer posible tambien leer matriz de distancias para construir un mapa
+# lee parametros
 #   tamaño , con o sin agujeros, simetrico o no
-#unir ciudades
+# unir ciudades
 #    o todas con todas
 #        simetricamente o asimetricamente(la distancia real * factor aleatorio pequeño
 #  o conectar con las mas cercana (un cierto radio) (comprobar despues la conexion total del grafo)
-#guardar en fichero con nombre indicando tamaño y tipo + num secuencia
+# guardar en fichero con nombre indicando tamaño y tipo + num secuencia
 # getters: distancia entre dos ciudades, numero de ciudades
-
--- src/tsp.py
+++ src/tsp.py
@@ -1,54 +1,75 @@
 #! /usr/bin/env python3.5
-#TODO arreglar el tema del paquete (__init__.py)
 from gen_map import Mapa
 from pickle import load
+
+
 class tsp_coa:
-    
-    def __init__(self,mapa):
-        self.mapa=mapa
-        nc=mapa.get_num_ciudades()
-        self.m_feromonas=[[0 for x in range(nc)]for i in range(nc)]
-        
-    def ejecutar(self, max_it, nh, algoritmo,np):
+    def __init__(self, mapa):
+        self.mapa = mapa
+        nc = mapa.get_num_ciudades()
+        self.m_feromonas = [[0 for x in range(nc)] for i in range(nc)]
+
+    def ejecutar(self, max_it, nh, algoritmo, np):
         for it in range(max_it):
-            caminos=[]
+            rutas = []
             for h in range(nh):
-                caminos.append(self.soltar_hormiga())
-            self.actualizar_feromonas(caminos)
-            self.evaporar_feromonas(caminos)
+                rutas.append(self.soltar_hormiga())
+            self.actualizar_feromonas(rutas)
+            self.evaporar_feromonas()
             if self.convergencia():
                 break
-    def soltar_hormiga(self):
-        #se parte siempre de la primera ciudad
-        camino=[0]
-        min=0
-        nc=self.mapa.get_num_ciudades()
-        #for i in range(nc):
-             
-        self.mapa.pib()
-        
-            
-    def actualizar_feromonas(self,a):
-        print(a)
-    def evaporar_feromonas(self,a):
-        print(a)
-    def convergencia(self):
-        return True
-if __name__ == '__main__':
-    #TODO
-    fichero="mapas/mapa.mp"
-    max_it=1
-    nh=5
-    algoritmo=0
-    np=1
-    
-    with open(fichero,'rb') as f:
-        mapa=load(f)
-    
-    t=tsp_coa(mapa)
-    t.ejecutar(max_it,nh,algoritmo,np)
 
-#main
+    def soltar_hormiga(self):
+        # se parte siempre de la primera ciudad
+        actual = 0
+        ruta = [actual]
+        nc = self.mapa.get_num_ciudades()
+        candidatas = list(range(1, nc))
+        for i in range(1, nc):
+            elegida = candidatas[0]
+            min_dist = self.mapa.get_distancia(actual, elegida)
+            for c in candidatas:
+                m = self.mapa.get_distancia(actual, c)
+                if min_dist > m:
+                    min_dist = m
+                    elegida = c
+            ruta.append(elegida)
+            candidatas.remove(elegida)
+        return ruta
+
+    def actualizar_feromonas(self, rutas):
+        nc = self.mapa.get_num_ciudades()
+        for r in rutas:
+            for i in range(nc):
+                origen = r[i]
+                destino = r[(i + 1) % nc]
+                self.m_feromonas[origen][destino] += 1
+
+    def evaporar_feromonas(self):
+        for origen in self.m_feromonas:
+            for destino in range(len(origen)):
+                if origen[destino] != 0:
+                    origen[destino] -= 1
+
+    def convergencia(self):
+
+        return False
+
+
+if __name__ == '__main__':
+    # TODO
+    fichero = "mapas/mapa.mp"
+    max_it = 1
+    nh = 5
+    algoritmo = 0
+    np = 1
+
+    with open(fichero, 'rb') as f:
+        mapa = load(f)
+    t = tsp_coa(mapa)
+    t.ejecutar(max_it, nh, algoritmo, np)
+
+# main
 #   leer parametros de configuracion
 #       mapa(de cualquier tipo) mismo tratamiento?????
 #       numero de hormigas
