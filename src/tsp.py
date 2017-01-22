17e887807216d4070e637caa2dc5976a2686db80
Andres Lopez Soleto <gordilocuente@gmail.com>
2017-01-22 03:13:31 +0100

    lanza hormigas chachi

--- src/tsp.py
+++ src/tsp.py
@@ -1,60 +1,83 @@
 #! /usr/bin/env python3.5
 from gen_map import Mapa
 from pickle import load
+from random import random
 
 
-class tsp_coa:
+class tsp_as:
     def __init__(self, mapa):
         self.mapa = mapa
-        nc = mapa.get_num_ciudades()
-        self.m_feromonas = [[0 for x in range(nc)] for i in range(nc)]
+        self.nc = mapa.get_num_ciudades()
+        self.m_feromonas = [[1 for x in range(self.nc)] for i in range(self.nc)]
 
-    def ejecutar(self, max_it, nh, algoritmo, np):
+    def ejecutar(self, max_it, nh, algoritmo, np, alfa, beta):
+        self.alfa=alfa
+        self.beta=beta
+        self.actualizar_g()
         for it in range(max_it):
             rutas = []
             for h in range(nh):
                 rutas.append(self.soltar_hormiga())
             self.actualizar_feromonas(rutas)
-            self.evaporar_feromonas()
             if self.convergencia():
                 break
 
     def soltar_hormiga(self):
         # se parte siempre de la primera ciudad
-        actual = 0
-        ruta = [actual]
-        nc = self.mapa.get_num_ciudades()
-        candidatas = list(range(1, nc))
-        for i in range(1, nc):
-            elegida = candidatas[0]
-            min_dist = self.mapa.get_distancia(actual, elegida)
-            for c in candidatas:
-                m = self.mapa.get_distancia(actual, c)
-                if min_dist > m:
-                    min_dist = m
-                    elegida = c
+        ruta = [0]
+        for i in range(1, self.nc):
+            elecciones = self.get_probobabilidades(ruta)
+            r = random()
+            p_acum = 0.0
+            elegida = None
+            for e in elecciones:
+                p_acum += e['p']
+                if r < p_acum:
+                    elegida = e['id']
+                    break
             ruta.append(elegida)
-            candidatas.remove(elegida)
         return ruta
 
     def actualizar_feromonas(self, rutas):
-        nc = self.mapa.get_num_ciudades()
-        for r in rutas:
-            for i in range(nc):
-                origen = r[i]
-                destino = r[(i + 1) % nc]
-                self.m_feromonas[origen][destino] += 1
+        self.evaporar_feromonas()
+
+
+
+        self.actualizar_g()
 
     def evaporar_feromonas(self):
         for origen in self.m_feromonas:
             for destino in range(len(origen)):
-                if origen[destino] != 0:
-                    origen[destino] -= 1
+                origen[destino] -= 1
+                if origen[destino] < 1:
+                    origen[destino]=1
 
     def convergencia(self):
 
         return False
 
+    def get_probobabilidades(self, ruta):
+        i = ruta[-1]
+        sumatorio_gi = 0.0
+        candidatas = []
+        for j in range(len(self.g[i])):
+            if j not in ruta:
+                sumatorio_gi += self.g[i][j]
+                candidatas.append(j)
+        probabilidades = []
+        for j in candidatas:
+            p = self.g[i][j] / sumatorio_gi
+            probabilidades.append({'id': j, 'p': p})
+        return probabilidades
+
+    def actualizar_g(self):
+        self.g=[[None]*self.nc for i in range(self.nc)]
+        for i in range(self.nc):
+            for j in range(self.nc):
+                if i != j:
+                    f_ij = self.m_feromonas[i][j]*self.alfa
+                    h_ij = self.mapa.get_distancia(i, j)*self.beta
+                    self.g[i][j] = f_ij/h_ij
 
 if __name__ == '__main__':
     # TODO
@@ -63,11 +86,12 @@ if __name__ == '__main__':
     nh = 5
     algoritmo = 0
     np = 1
-
+    alfa=1
+    beta=1
     with open(fichero, 'rb') as f:
         mapa = load(f)
-    t = tsp_coa(mapa)
-    t.ejecutar(max_it, nh, algoritmo, np)
+    t = tsp_as(mapa)
+    t.ejecutar(max_it, nh, algoritmo, np, alfa, beta)
 
 # main
 #   leer parametros de configuracion
